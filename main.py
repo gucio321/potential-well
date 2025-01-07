aa99c80f865f2c009d91332eb647f9ea6806c9b0
gucio321 <gucio321@protonmail.com>
2025-01-07 13:00:49 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def read_input():
+    
+
+def main():
+    data = read_input()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
