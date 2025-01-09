35aa5a558fee646634ecf91ac61e162adb594960
gucio321 <gucio321@protonmail.com>
2025-01-09 18:16:28 +0100

    why the hell this doesn't work

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ class PotentialWellSymulator:
         self.__IMG_DATA = [1.0,1.0,1.0,1.0] * self.__IMG_SIZE[0] * self.__IMG_SIZE[1]
         self.__DPI = 100
 
+        self.x = numpy.linspace(0, A, int(A/dx))
+        self.time = 0
+
     def run(self):
         """
         run handles UI things. Should be called once per class instance (even once per script).
@@ -61,6 +64,8 @@ class PotentialWellSymulator:
         # do plotting
         plt.plot([0,0], [-10,10], 'k-')
         plt.plot([A,A], [-10,10], 'k-')
+        self.time += imgui.get_delta_time()
+        # plt.plot(self.x,[psi(A,3,x,self.time) for x in self.x])
 
         # now a bit of python magic to export figure to imgui texture
         canvas = FigureCanvas(fig)
@@ -74,11 +79,11 @@ class PotentialWellSymulator:
         # push to registry
         imgui.set_value("texture_tag", image)
 
-# def psi(a, n, x, t):
-#     return a*math.sin(n*math.pi*x/A)
-#
-#     duration=100
-#     x = numpy.linspace(0, A, int(A/dx))
+def psi(a, n, x, t):
+    return a*math.sin(n*math.pi*x/A)
+
+    # duration=100
+    # x = numpy.linspace(0, A, int(A/dx))
     # myPlot, = plt.plot(x, [0 for _ in range(len(x))])
     # for i in range(duration):
     #     myPlot.set_ydata([psi(A,3,x,i*DELTA_TIME) for x in x])
