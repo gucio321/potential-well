a727f7136429e98bd87e1d4808e6fcc00a04bbf9
gucio321 <gucio321@protonmail.com>
2025-01-10 10:40:52 +0100

    main: add a input field

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import math
 import numpy
 import dearpygui.dearpygui as imgui
 
-DELTA_TIME = 60 # ms
-A = 10
-dx = 0.01
-
 class PotentialWellSymulator:
     def __init__(self):
         self.__WINDOW_SIZE = (800, 600)
         self.__TITLE = "Potential Well Simulation"
         self.__PRIMARY_WINDOW_ID = 'Primary Window' # this will be hidden as title bar is hidden.
         self.__IMG_SIZE = (400, 400)
+        self.__A_SLIDER_ID = "a_slider_id"
+        self.dx = 0.01 # sets plotting accuracy (the higher the smoother plot is and more resources are used)
 
-        self.x = numpy.linspace(0, A, int(A/dx))
+        self._a = 0
+        self.x = []
         self.n = 1
         self.time = 0
+
+    def __initialize(self):
+        self.a = 10
 
     def run(self):
         """
@@ -29,6 +31,7 @@ class PotentialWellSymulator:
         imgui.create_context()
         imgui.create_viewport(title='Custom Title', width=self.__WINDOW_SIZE[0], height=self.__WINDOW_SIZE[1])
         self.render()
+        self.__initialize()
         imgui.setup_dearpygui()
         imgui.show_viewport()
         imgui.set_primary_window(self.__PRIMARY_WINDOW_ID, True)
@@ -50,7 +53,8 @@ class PotentialWellSymulator:
                 # first ploting iteration
                 self.plot()
 
-            imgui.add_slider_int(label="n", max_value=10, callback=lambda _,value : self._set_n(value))
+            imgui.add_slider_int(label="n",default_value=self.n, max_value=10, callback=lambda _,value : self._set_n(value))
+            imgui.add_input_int(tag=self.__A_SLIDER_ID, label="Szerokość studni A", default_value=self.a, callback=lambda _, value: self.__set_a(value))
 
     def plot(self):
         """
@@ -58,15 +62,29 @@ class PotentialWellSymulator:
         :return:
         """
         imgui.set_value('left_wall', [[0,0], [-10,10]])
-        imgui.set_value('right_wall',[[A,A], [-10,10]])
-        imgui.set_value('psi', [self.x, [psi(1, self.n, X, self.time) for X in self.x]])
+        imgui.set_value('right_wall',[[self.a,self.a], [-10,10]])
+        imgui.set_value('psi', [self.x, [self.psi(1, self.n, X, self.time) for X in self.x]])
 
     def _set_n(self, n):
         self.n = n
         self.plot() # replot
 
-def psi(a, n, x, t):
-    return a*math.sin(n*math.pi*x/A)
+    def __set_a(self, a):
+        self.a = a
+
+    @property
+    def a(self):
+        return self._a
+
+    @a.setter
+    def a(self, new_a):
+        self._a = new_a
+        self.x = numpy.linspace(0, new_a, int(new_a/self.dx))
+        self.plot()
+        imgui.set_value(self.__A_SLIDER_ID, new_a)
+
+    def psi(self, a, n, x, t):
+        return a*math.sin(n*math.pi*x/self.a)
 
 def main():
     sim = PotentialWellSymulator()
