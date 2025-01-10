0841f623ebf5eb0cc6f18cde132e329086702985
gucio321 <gucio321@protonmail.com>
2025-01-10 10:49:40 +0100

    rename a to width

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ class PotentialWellSymulator:
         self.__WINDOW_SIZE = (800, 600)
         self.__TITLE = "Potential Well Simulation"
         self.__PRIMARY_WINDOW_ID = 'Primary Window' # this will be hidden as title bar is hidden.
-        self.__IMG_SIZE = (400, 400)
-        self.__A_SLIDER_ID = "a_slider_id"
+        self.__IMG_SIZE = (-1, 400)
+        self.__W_SLIDER_ID = "width_slider_id"
         self.dx = 0.01 # sets plotting accuracy (the higher the smoother plot is and more resources are used)
 
-        self._a = 0
+        self._width = 0
         self.x = []
         self.n = 1
         self.time = 0
 
     def __initialize(self):
-        self.a = 10
+        self.width = 10
 
     def run(self):
         """
@@ -54,7 +54,7 @@ class PotentialWellSymulator:
                 self.plot()
 
             imgui.add_slider_int(label="n",default_value=self.n, max_value=10, callback=lambda _,value : self._set_n(value))
-            imgui.add_input_int(tag=self.__A_SLIDER_ID, label="Szerokość studni A", default_value=self.a, callback=lambda _, value: self.__set_a(value))
+            imgui.add_input_int(tag=self.__W_SLIDER_ID, label="Szerokość studni A", default_value=self.width, callback=lambda _, value: self.__set_a(value))
 
     def plot(self):
         """
@@ -62,29 +62,29 @@ class PotentialWellSymulator:
         :return:
         """
         imgui.set_value('left_wall', [[0,0], [-10,10]])
-        imgui.set_value('right_wall',[[self.a,self.a], [-10,10]])
+        imgui.set_value('right_wall',[[self.width,self.width], [-10,10]])
         imgui.set_value('psi', [self.x, [self.psi(1, self.n, X, self.time) for X in self.x]])
 
     def _set_n(self, n):
         self.n = n
         self.plot() # replot
 
-    def __set_a(self, a):
-        self.a = a
+    def __set_w(self, w):
+        self.width = w
 
     @property
-    def a(self):
-        return self._a
+    def width(self):
+        return self._width
 
-    @a.setter
-    def a(self, new_a):
-        self._a = new_a
-        self.x = numpy.linspace(0, new_a, int(new_a/self.dx))
+    @width.setter
+    def width(self, w):
+        self._width = w
+        self.x = numpy.linspace(0, w, int(w/self.dx))
         self.plot()
-        imgui.set_value(self.__A_SLIDER_ID, new_a)
+        imgui.set_value(self.__W_SLIDER_ID, w)
 
     def psi(self, a, n, x, t):
-        return a*math.sin(n*math.pi*x/self.a)
+        return a*math.sin(n*math.pi*x/self.width)
 
 def main():
     sim = PotentialWellSymulator()
