84a7083bec81442b6b0b7cfdaa644d1e3788ac9d
gucio321 <gucio321@protonmail.com>
2025-01-10 10:10:43 +0100

    Add reploting mechanism

--- main.py
+++ main.py
@@ -17,9 +17,8 @@ dx = 0.01
 class PotentialWellSymulator:
     def __init__(self):
         self.__WINDOW_SIZE = (800, 600)
+        self.__TITLE = "Potential Well Simulation"
         self.__IMG_SIZE = (400, 400)
-        self.__IMG_DATA = [1.0,1.0,1.0,1.0] * self.__IMG_SIZE[0] * self.__IMG_SIZE[1]
-        self.__DPI = 100
 
         self.x = numpy.linspace(0, A, int(A/dx))
         self.n = 1
@@ -45,6 +44,15 @@ class PotentialWellSymulator:
         self.time += imgui.get_delta_time()
         with imgui.window(label="Tutorial", no_move=True, no_title_bar=True, no_collapse=True, no_scrollbar=True,no_resize=True):
             with imgui.plot(label="Symulacja", width=self.__IMG_SIZE[0], height=self.__IMG_SIZE[1]):
+                imgui.add_plot_axis(imgui.mvXAxis, label="x")
+                imgui.add_plot_axis(imgui.mvYAxis, label="y", tag="y_axis")
+
+                # initialize plots
+                imgui.add_line_series([], [], tag='left_wall', parent='y_axis')
+                imgui.add_line_series([], [], tag='right_wall', parent='y_axis')
+                imgui.add_line_series([], [], tag='psi', parent='y_axis')
+
+                # first ploting iteration
                 self.plot()
 
             imgui.add_slider_int(label="n", max_value=10, callback=lambda _,value : self._set_n(value))
@@ -54,14 +62,13 @@ class PotentialWellSymulator:
         plot is supposed to update plot texture to the atlas.
         :return:
         """
-        imgui.add_plot_axis(imgui.mvXAxis, label="x")
-        imgui.add_plot_axis(imgui.mvYAxis,label="y", tag="y_axis")
-        imgui.add_line_series([0,0], [-10,10], parent="y_axis")
-        imgui.add_line_series([A,A], [-10,10], parent="y_axis")
-        imgui.add_line_series(self.x, [psi(1, self.n, X, self.time) for X in self.x], parent="y_axis")
+        imgui.set_value('left_wall', [[0,0], [-10,10]])
+        imgui.set_value('right_wall',[[A,A], [-10,10]])
+        imgui.set_value('psi', [self.x, [psi(1, self.n, X, self.time) for X in self.x]])
 
     def _set_n(self, n):
         self.n = n
+        self.plot() # replot
 
 def psi(a, n, x, t):
     return a*math.sin(n*math.pi*x/A)
